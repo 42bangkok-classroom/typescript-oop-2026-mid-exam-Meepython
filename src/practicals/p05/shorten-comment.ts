24ab81f1f2f16753d8972ef85c249443f877ae68
Meepython <phookankawee@gmail.com>
2026-01-21 16:18:33 +0700

    warw

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,6 @@
-const comment = "TypeScript is a strongly typed programming language"
-
+export {}
+//const comment = "TypeScript is a strongly typed programming language"
+const comment = "aaaaa aaaaaaaaaa"
 export function shortenComment(comment: string):string { 
   // Write your code below
   let stringArray:string[] = comment.split(" ")
@@ -7,7 +8,6 @@ export function shortenComment(comment: string):string {
   let order:number = 0
   for (let index = 0; index < stringArray.length; index++) {
     if (stringArray[index].length <= 10 && stringArray[index].length >= 5) {
-      //result.concat(stringArray[index])
       if (order !== 1) {
         order++
         result = result + stringArray[index]
@@ -15,10 +15,12 @@ export function shortenComment(comment: string):string {
         result = result + " " + stringArray[index]
       }
     } else {
-      return ""
+      
     }
   }
-  return result
+  if (result === "") {
+    return ""
+  } else {
+    return result
+  }
 }
-
-shortenComment(comment)
