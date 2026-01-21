b95765cc8ba4f5ae596fa420d4f96ae3a87a89b6
Meepython <phookankawee@gmail.com>
2026-01-21 15:26:35 +0700

    test

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,18 @@
 // Write your code below
+const size:number = Number(process.argv[2])
+
+if (Number.isNaN(size) || size === null || size <= 0) {
+    process.exit(0)
+}
+
+let order:number = 0
+
+// Row /
+
+for (let i = size; i >= 1; i--) {
+  let row = "";
+  for (let j = i; j >= 1; j--) {
+    row += j;
+  }
+  console.log(row);
+}
--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,11 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
+function getUniqueNumbers(arr1: number[], arr2: number[]): void { //number[]
   // Write your code below
+  const newArray: number[] = [... new Set([...arr1, ...arr2])]
+  console.log(newArray)
+  
 }
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+
+getUniqueNumbers(arr1, arr2)
